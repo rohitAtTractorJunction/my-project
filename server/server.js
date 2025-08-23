2a0b23a65abe260def3df9124834d0c8dbaef61a
Rohit95286 <rohitjoshi80061@gmail.com>
2025-08-23 16:11:09 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,21 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { initializeConnection } = require("./config/database");
+const {initializeConnection} = require("./config/database");
 const path = require("path");
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-  ],
-  methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
-
 app.get("*", (_, res) => {
   res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
 });
@@ -28,7 +27,7 @@ const purchasedCoursesRoutes = require("./routes/purchasedCoursesRoutes");
 const storePurchaseRoutes = require("./routes/storePurchaseRoutes");
 const courseContentRoutes = require("./routes/courseContentRoutes");
 
-app.use("/auth", authRoutes); 
+app.use("/auth", authRoutes);
 app.use("/checkout", checkoutRoutes);
 app.use("/courses", courseRoutes);
 app.use("/videos", videoRoutes);
@@ -36,14 +35,11 @@ app.use("/purchased", purchasedCoursesRoutes);
 app.use("/store-purchase", storePurchaseRoutes);
 app.use("/course-content", courseContentRoutes);
 
-
 initializeConnection()
-  .then((connection) => {
+  .then(connection => {
     const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  }) 
-  .catch((err) => {
+    app.listen(port, () => {});
+  })
+  .catch(err => {
     console.error("Error initializing database connection:", err);
   });
