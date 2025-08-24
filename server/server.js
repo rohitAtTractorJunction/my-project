0de7ce50f0173da2f53c7929f071718fe1e03795
Rohit95286 <rohitjoshi80061@gmail.com>
2025-08-24 09:58:59 +0530

    gtihub actions

--- server/server.js
+++ server/server.js
@@ -47,5 +47,5 @@ app.use("/course-content", courseContentRoutes);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on rt ${port}`);
 });
